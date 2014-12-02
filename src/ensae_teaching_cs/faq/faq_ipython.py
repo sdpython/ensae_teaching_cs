f0b51f72af872ea45475bb7c907c84d4a9f0de98
sdpython <xavier.dupre@gmail.com>
2014-12-02 22:26:37 +0100

    FAQ autour de ipython

---
+++ src/ensae_teaching_cs/faq/faq_ipython.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+@file
+@brief Quelques problèmes récurrents avec `IPython <http://ipython.org/>`_.
+
+"""
+
+def notebook_path():
+    """
+
+    change matplotlib style
+
+    @param      style       style
+
+    @FAQ(ipython___Récupérer le fichier du notebook depuis le notebook)
+
+    Voir `How to I get the current IPython Notebook name <http://stackoverflow.com/questions/12544056/how-to-i-get-the-current-ipython-notebook-name>`_
+    
+    Il suffit d'insérer la cellule suivante dans le notebook ::
+
+        %%javascript
+        var kernel = IPython.notebook.kernel;
+        var body = document.body,  
+            attribs = body.attributes;
+        var command = "theNotebook = os.path.join(" + "r'"+attribs['data-project'].value+"'," + "r'"+attribs['data-notebook-path'].value+"'," + "r'"+attribs['data-notebook-name'].value+"')";
+        kernel.execute(command);
+        
+    On peut vérifier que cela fonctionne ::
+    
+        theNotebook
+
+    @endFAQ
+    """
+    pass
