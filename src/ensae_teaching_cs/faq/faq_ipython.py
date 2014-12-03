788713731c13b96c805d0eed67312e3cd3dcf195
sdpython <xavier.dupre@gmail.com>
2014-12-03 23:23:50 +0100

    correction de la séance 7

--- src/ensae_teaching_cs/faq/faq_ipython.py
+++ src/ensae_teaching_cs/faq/faq_ipython.py
@@ -15,18 +15,18 @@ def notebook_path():
     @FAQ(ipython___Récupérer le fichier du notebook depuis le notebook)
 
     Voir `How to I get the current IPython Notebook name <http://stackoverflow.com/questions/12544056/how-to-i-get-the-current-ipython-notebook-name>`_
-    
+
     Il suffit d'insérer la cellule suivante dans le notebook ::
 
         %%javascript
         var kernel = IPython.notebook.kernel;
-        var body = document.body,  
+        var body = document.body,
             attribs = body.attributes;
         var command = "theNotebook = os.path.join(" + "r'"+attribs['data-project'].value+"'," + "r'"+attribs['data-notebook-path'].value+"'," + "r'"+attribs['data-notebook-name'].value+"')";
         kernel.execute(command);
-        
+
     On peut vérifier que cela fonctionne ::
-    
+
         theNotebook
 
     @endFAQ
