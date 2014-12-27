3cdbf41e98fb16f453793a35062f765e48b75590
dupre <xavier.dupre@ensae.fr>
2014-12-27 23:09:19 +0100

    remove extra space

--- _unittests/ut_td_2a/test_serialize.py
+++ _unittests/ut_td_2a/test_serialize.py
@@ -28,32 +28,32 @@ class TestSerialization (unittest.TestCase):
 
     def test_serialize(self) :
         fLOG (__file__, self._testMethodName, OutputPrint = __name__ == "__main__")
-        
+
         temp = os.path.abspath(os.path.dirname(__file__))
         temp = os.path.join(temp, "temp_serialization")
         if not os.path.exists(temp):
             os.mkdir(temp)
-        
+
         df = pandas.DataFrame( [ {"name":"xavier", "school":"ENSAE"},
                                  {"name":"antoine", "school":"ENSAE"} ] )
-                                 
+
         outfile = os.path.join(temp, "out_df.bin")
         if os.path.exists(outfile): os.remove(outfile)
-        
+
         dump_object(df, outfile)
         assert os.path.exists(outfile)
-        
+
         df2 = load_object(outfile)
         fLOG(df2)
-        fLOG(df2.values.tolist())        
+        fLOG(df2.values.tolist())
         assert df.values.tolist() == df2.values.tolist()
-        
+
         s = io.BytesIO()
         dump_object(df, s)
         s.seek(0)
         df3 = load_object(s)
         assert df.values.tolist() == df3.values.tolist()
-        
+
 
 if __name__ == "__main__"  :
     unittest.main ()
