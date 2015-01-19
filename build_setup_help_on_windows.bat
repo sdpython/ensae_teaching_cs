2b6d9ac08195df53f4d128fd95f037d700b0b829
dupre <xavier.dupre@ensae.fr>
2015-01-19 15:59:19 +0100

    fix setup + update batch

--- build_setup_help_on_windows.bat
+++ build_setup_help_on_windows.bat
@@ -42,41 +42,42 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
-:setup33_64:
-IF NOT EXIST c:\Python33_x64 GOTO setup33:
-set pythonexe="c:\Python33_x64\python"
-%pythonexe% setup.py clean_pyd
-%pythonexe% setup.py build bdist_wininst --plat-name=win-amd64
-if %errorlevel% neq 0 exit /b %errorlevel%
-echo #######################################################
-
 :setup33:
-IF NOT EXIST c:\Python33 GOTO setup34_64:
+IF NOT EXIST c:\Python33 GOTO setup33_64:
 set pythonexe="c:\Python33\python"
 %pythonexe% setup.py clean_pyd
-%pythonexe% setup.py sdist --formats=gztar,zip --verbose
-if %errorlevel% neq 0 exit /b %errorlevel%
 %pythonexe% setup.py bdist_wininst
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
-:setup34_64:
-set pythonexe="c:\Python34_x64\python"
+:setup33_64:
+IF NOT EXIST c:\Python33_x64 GOTO setup34:
+set pythonexe="c:\Python33_x64\python"
+%pythonexe% setup.py clean_pyd
+%pythonexe% setup.py sdist --formats=gztar,zip --verbose
+if %errorlevel% neq 0 exit /b %errorlevel%
+%pythonexe% setup.py build bdist_wininst --plat-name=win-amd64
+if %errorlevel% neq 0 exit /b %errorlevel%
+echo #######################################################
+
+:setup34:
+IF NOT EXIST c:\Python34 GOTO setup34_x64_msi_wheel:
+set pythonexe="c:\Python34\python"
 %pythonexe% setup.py clean_pyd
 %pythonexe% setup.py build bdist_wininst --plat-name=win-amd64
 if %errorlevel% neq 0 exit /b %errorlevel%
-%pythonexe% setup.py build bdist_msi --plat-name=win-amd64
-if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
-:setup_msi:
+:setup34_x64_msi_wheel:
 set pythonexe="c:\Python34_x64\python"
 %pythonexe% setup.py clean_pyd
 %pythonexe% setup.py sdist --formats=gztar,zip --verbose
 if %errorlevel% neq 0 exit /b %errorlevel%
-%pythonexe% setup.py bdist_wininst
+%pythonexe% setup.py bdist_wininst --plat-name=win-amd64
 if %errorlevel% neq 0 exit /b %errorlevel%
 %pythonexe% setup.py bdist_msi
+if %errorlevel% neq 0 exit /b %errorlevel%
+%pythonexe% setup.py bdist_wheel
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
