b131345fa11082a4366317a79d94c56e7d422385
dupre <xavier.dupre@ensae.fr>
2015-02-16 00:00:19 +0100

    new visit of the index

--- src/ensae_teaching_cs/automation/ftp_publish_helper.py
+++ src/ensae_teaching_cs/automation/ftp_publish_helper.py
@@ -10,17 +10,17 @@ from pyquickhelper.filehelper.ftp_transfer_files import content_as_binary as pqh
 
 def trigger_on_specific_strings(content):
     """
-    look for specific string such as *USERNAME*, *USERDNSDOMAIN*, *HOMEPATH*, *USERNAME*,
-                *COMPUTERNAME*, *LOGONSERVER*, and returns None if it was found
-    or modifies the content to remove it
+    look for specific string such as
+    *USERNAME*, *USERDNSDOMAIN*, *HOMEPATH*, *USERNAME*, *COMPUTERNAME*, *LOGONSERVER*,
+    and returns None if it was found or modifies the content to remove it
     """
     strep = [(r"C:\\%s\\__home_\\_data\\" % os.environ["USERNAME"], "somewhere"),
-              ("C:\\%s\\__home_\\_data\\" % os.environ["USERNAME"], "somewhere"),  
+              ("C:\\%s\\__home_\\_data\\" % os.environ["USERNAME"], "somewhere"),
               ]
     for s,b in strep:
         if s in content:
             content = content.replace(s, b)
-    
+
     lower_content = content.lower()
     for st in ["USERNAME", "USERDNSDOMAIN", "HOMEPATH", "USERNAME",
                 "COMPUTERNAME", "LOGONSERVER"]:
@@ -29,12 +29,12 @@ def trigger_on_specific_strings(content):
             raise Exception("string {0}:{1} was found".format(st, s))
             return None
     return content
-    
+
 def content_as_binary(filename):
     """
-    overloads function `content_as_finary <http://www.xavierdupre.fr/app/pyquickhelper/helpsphinx/pyquickhelper/filehelper/ftp_transfer_files.html?highlight=content_as_binary#pyquickhelper.filehelper.ftp_transfer_files.content_as_binary>`_ from 
+    overloads function `content_as_finary <http://www.xavierdupre.fr/app/pyquickhelper/helpsphinx/pyquickhelper/filehelper/ftp_transfer_files.html?highlight=content_as_binary#pyquickhelper.filehelper.ftp_transfer_files.content_as_binary>`_ from
     `pyquickhelper <http://www.xavierdupre.fr/app/pyquickhelper/helpsphinx/pyquickhelper/">`_
-    
+
     determines if filename is binary or None before transfering it
 
     @param      filename        filename
@@ -59,9 +59,9 @@ def publish_documentation(
                 ):
     """
     publish the documentation and the setups of a python module on a webiste,
-    it assumes the modules is organized the same way as 
+    it assumes the modules is organized the same way as
     `pyquickhelper <http://www.xavierdupre.fr/app/pyquickhelper/helpsphinx/index.html>`_.
-    
+
     @param      docs            list of dictionaries (see below)
     @param      ftpsite         something like ``ftp.something.``
     @param      login           login
@@ -72,24 +72,24 @@ def publish_documentation(
                                 appies only on text files
     @param      is_binary       a function to tell if a content of a file is binary or not
     @param      fLOG            logging function
-    
+
     *docs* is a list of dictionaries which must contain for each folder
     to transfer:
         - ``local``: local folder
         - ``root_local``: local paths will be related to this root
         - ``root_web``: prefix to add to the remote paths
         - ``status_file``: a file where the function populates the transfered files and some information about them
-        
+
     A local file is composed by ``<local_root>/<relative_path>``, it
     will be uploaded to ``<web_root>/<relative_path>``.
-    
+
     If one of the three first parameters is None, the function
     will open a popup windows to ask the missing information.
     See `open_window_params <http://www.xavierdupre.fr/app/pyquickhelper/helpsphinx/pyquickhelper/funcwin/frame_params.html#pyquickhelper.funcwin.frame_params.open_window_params>`_.
-    
+
     Here is an example of a program which pusblishes several documentations on the
     same website::
-    
+
         from pyquickhelper import TransferFTP, FileTreeNode, FolderTransferFTP, open_window_params
         from ensae_teaching_cs.automation.ftp_publish_helper import publish_documentation
 
@@ -122,60 +122,60 @@ def publish_documentation(
                 "python_project_template",
                 "ensae_teaching_cs",
                 ] :
-                    
+
             root = os.path.abspath(location % module)
             project = dict ( status_file = os.path.join(this, "status_%s.txt" % module),
                              local = root,
                              root_local = root,
                              root_web = rootw % module)
             projects.append (project)
-            
+
         publish_documentation   (projects,
                         ftpsite         = "ftp.something.cc",
                         login           = login,
                         password        = password,
                         key_save        = "my_module_password",
                         footer_html     = footer)
-    
-    
-    
+
+
+
     """
-                    
+
     params = {"ftpsite":ftpsite,
               "login":login,
               "password":password,
               }
-              
+
     nbnone = len ( [ v for k,v in params.items() if v is None or len(v) == 0 ] )
     if nbnone > 0:
         params = open_window_params (params, title="Website and Credentials", help_string = "ftp site + login + password", key_save=key_save)
-        
+
     nbnone = [ v for k,v in params.items() if v is None or len(v) == 0 ]
     if len(nbnone) > 0:
         raise Exception("one of the parameters is None:\n" + str(nbnone))
-        
+
     password = params["password"]
     login = params["login"]
     ftpsite = params["ftpsite"]
-    
-    ftp = TransferFTP(ftpsite, 
-                    login, 
-                    password, 
+
+    ftp = TransferFTP(ftpsite,
+                    login,
+                    password,
                     fLOG=fLOG)
-                    
+
     for project in docs:
-        
+
         location = project["local"]
         root_local = project["root_local"]
         root_web = project["root_web"]
-                
+
         fLOG("-------------------------",location)
 
         sfile = project["status_file"]
         rootw = project["root_web"]
-        
+
         ftn  = FileTreeNode(root_local)
-        fftp = FolderTransferFTP (ftn, ftp, sfile, 
+        fftp = FolderTransferFTP (ftn, ftp, sfile,
                             root_web = rootw,
                             fLOG=fLOG,
                             footer_html = footer_html,
@@ -183,10 +183,10 @@ def publish_documentation(
                             is_binary=is_binary)
 
         fftp.start_transfering()
-        
-        ftn  = FileTreeNode(os.path.join(root_local,".."), 
+
+        ftn  = FileTreeNode(os.path.join(root_local,".."),
                             filter = lambda root, path, f, dir: not dir)
-        fftp = FolderTransferFTP (ftn, ftp, sfile, 
+        fftp = FolderTransferFTP (ftn, ftp, sfile,
                             root_web = root_web.replace("helpsphinx",""),
                             fLOG=fLOG,
                             footer_html = footer_html,
@@ -194,7 +194,6 @@ def publish_documentation(
                             is_binary=is_binary)
 
         fftp.start_transfering()
-        
 
-    ftp.close()    
 
+    ftp.close()
