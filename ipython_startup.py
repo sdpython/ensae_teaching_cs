b13be34a737117744cb9decb5437cd4570a0b8c8
dupre <xavier.dupre@ensae.fr>
2015-03-24 16:51:29 +0100

    add code_beatrix

--- ipython_startup.py
+++ ipython_startup.py
@@ -2,6 +2,7 @@ import os
 import sys
 path = os.path.abspath(os.path.dirname(__file__))
 for p in ["pyensae", "pyquickhelper", "ensae_teaching_cs",
-          "pymyinstall", "pyrsslocal", "pymmails", "pysqllike"]:
+          "pymyinstall", "pyrsslocal", "pymmails", "pysqllike",
+          "code_beatrix", "actuariat_python"]:
     pa = os.path.normpath(os.path.join(path, "..", p, "src"))
     sys.path.append(pa)
