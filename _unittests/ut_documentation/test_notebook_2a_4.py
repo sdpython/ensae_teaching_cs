c62a9e4798f22339955cbe5af5fce0ead631a1d9
dupre <xavier.dupre@ensae.fr>
2015-04-20 22:46:11 +0200

    split a unit test into two

--- _unittests/ut_documentation/test_notebook_2a_4.py
+++ _unittests/ut_documentation/test_notebook_2a_4.py
@@ -74,19 +74,43 @@ from src.ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, 
 
 class TestNotebookRunner2a_4 (unittest.TestCase):
 
-    def test_notebook_runner(self):
+    def test_notebook_runner_enonce(self):
         fLOG(
             __file__,
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
-        temp = get_temp_folder(__file__, "temp_notebook2a_4")
+        temp = get_temp_folder(__file__, "temp_notebook2a_4_enonce")
         keepnote = ls_notebooks("td2a")
         assert len(keepnote) > 0
         res = execute_notebooks(
             temp,
             keepnote,
             lambda i,
-            n: "_4" in n,
+            n: "_4" in n and "enonce" in n,
+            fLOG=fLOG)
+        assert len(res) > 0
+        fails = [(os.path.split(k)[-1], v)
+                 for k, v in sorted(res.items()) if not v[0]]
+
+        for f in fails:
+            fLOG("******************** NB FAILED", f[0])
+            fLOG(f)
+        if len(fails) > 0:
+            raise fails[0][1][1]
+
+    def test_notebook_runner_correction(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+        temp = get_temp_folder(__file__, "temp_notebook2a_4_correction")
+        keepnote = ls_notebooks("td2a")
+        assert len(keepnote) > 0
+        res = execute_notebooks(
+            temp,
+            keepnote,
+            lambda i,
+            n: "_4" in n and "correction" in n,
             fLOG=fLOG)
         assert len(res) > 0
         fails = [(os.path.split(k)[-1], v)
