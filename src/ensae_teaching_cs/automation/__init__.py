2fd5958ad6462864b867583821bacb5b177ef6d6
dupre <xavier.dupre@ensae.fr>
2015-04-26 01:20:20 +0200

    add a file opening RSS blogs for this modules and the related ones

---
+++ src/ensae_teaching_cs/automation/__init__.py
@@ -0,0 +1,9 @@
+"""
+@file
+@brief Shortcuts for automation
+"""
+
+from .jenkins_helper import setup_jenkins_server
+from .ftp_publish_helper import publish_documentation
+from .notebook_test_helper import execute_notebooks
+from .rss_blog import rss_teachings_update_run_server
