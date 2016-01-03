747c9634fef6aa50ddccb2a7d30c2478788538d5
dupre <xavier.dupre@ensae.fr>
2016-01-03 02:31:57 +0100

    update setup.py with binaries for Python 3.5

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ package_data = {project_var_name + ".pythonnet.py33": ["*.pyd", "*.txt", "*.dll"
                 project_var_name + ".pythonnet.py34": ["*.pyd", "*.txt", "*.dll"],
                 project_var_name + ".pythonnet.py34x64": ["*.pyd", "*.txt", "*.dll"],
                 project_var_name + ".pythonnet.py27x64": ["*.pyd", "*.txt", "*.dll"],
+                project_var_name + ".pythonnet.py35x64": ["*.pyd", "*.txt", "*.dll"],
                 project_var_name + ".pythonnet.csdll": ["*.dll"],
                 project_var_name + ".automation": ["*.xml", "*.r", "*.ico"],
                 project_var_name: ["rss_teachings.xml"],
