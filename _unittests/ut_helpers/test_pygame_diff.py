513f51ec018fe7e16262dfd99eb201fa3febe1f5
dupre <xavier.dupre@ensae.fr>
2016-04-24 15:22:00 +0200

    move functions from unit test to module

---
+++ _unittests/ut_helpers/test_pygame_diff.py
@@ -0,0 +1,99 @@
+"""
+@brief      test log(time=1s)
+
+You should indicate a time in seconds. The program ``run_unittests.py``
+will sort all test files by increasing time and run them.
+"""
+
+
+import sys
+import os
+import unittest
+import random
+from difflib import SequenceMatcher
+
+
+try:
+    import src
+    import pyquickhelper as skip_
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+    import pyquickhelper.loghelper as skip_
+
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import get_temp_folder
+from src.ensae_teaching_cs.helpers.pygame_helper import build_diff_image, get_pygame_screen_font
+
+
+class TestDiff(unittest.TestCase):
+
+    def test_diff(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        seq1 = "ab ab2 abc3 abcd abc4".split()
+        seq2 = "ab ab2 abc3 abc4 abc adb".split()
+        diff = SequenceMatcher(a=seq1, b=seq2)
+        nb = 0
+        for opcode in diff.get_opcodes():
+            fLOG(opcode)
+            nb += 1
+        self.assertEqual(nb, 4)
+
+        h = 20
+        size = 500, 500
+        white = 255, 255, 255
+
+        pygame, screen, fonts = get_pygame_screen_font(h, size)
+
+        from src.ensae_teaching_cs.helpers.pygame_helper import wait_event
+
+        bars = [random.randint(10, 500) / 500.0 for s in seq2]
+        screen.fill(white)
+        build_diff_image(pygame, screen, h=h, maxw=size[1], seq1=seq1, seq2=seq2, diff=diff,
+                         fonts=fonts, bars=bars)
+        pygame.display.flip()
+        temp = get_temp_folder(__file__, "temp_video_diff")
+
+        for i in range(0, 21):
+            screen.fill(white)
+            build_diff_image(pygame, screen, h=h, maxw=size[0], seq1=seq1, seq2=seq2, diff=diff,
+                             fonts=fonts, bars=bars, progress=i / 20.0, prev_bars=None)
+            pygame.time.wait(60)
+            pygame.display.flip()
+            pygame.image.save(screen, os.path.join(temp, "diff%d.png" % i))
+
+        if __name__ == "__main__":
+
+            from src.ensae_teaching_cs.helpers.video_helper import make_video
+            png = [os.path.join(temp, _)
+                   for _ in os.listdir(temp) if ".png" in _]
+            out = os.path.join(temp, "diff.avi")
+            make_video(png, out, size=(350, 250), format="XVID", fps=5)
+
+            wait_event(pygame)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/ensae_teaching_cs/helpers/pygame_helper.py
+++ src/ensae_teaching_cs/helpers/pygame_helper.py
@@ -6,6 +6,8 @@ The module pygame is not imported in this module but sent
 to every function as a parameter to avoid importing
 the module if not needed.
 """
+import math
+
 
 MOUSE = "mouse"
 KEY = "key"
@@ -45,3 +47,187 @@ def empty_main_loop(pygame):
         if event.type == pygame.MOUSEBUTTONUP:
             wait_event(pygame)
     return True
+
+
+def get_pygame_screen_font(h, size):
+    """
+    creates a surface with pygame, initialize the module, creates font
+
+    @param      h       size of the main font
+    @param      size    screen size
+    @return             pygame, screen, dictionary of fonts
+
+    The dictionary of fonts contains three fonts of size *h*,
+    *3h/4*, *5h/6*.
+    """
+    import pygame
+    pygame.init()
+    font = pygame.font.Font("freesansbold.ttf", h)
+    font_small = pygame.font.Font("freesansbold.ttf", 3 * h // 4)
+    screen = pygame.display.set_mode(size)
+    font = pygame.font.Font("freesansbold.ttf", h)
+    font_small = pygame.font.Font("freesansbold.ttf", 3 * h // 4)
+    font_half = pygame.font.Font("freesansbold.ttf", 5 * h // 6)
+    return pygame, screen, dict(font=font, font_half=font_half, font_small=font_small)
+
+
+def build_diff_image(pygame, screen, h, maxw, seq1=None, seq2=None, diff=None, fonts=None,
+                     bars=None, colors=None, progress=None, prev_bars=None):
+    """
+    build an image to show a difference between two lists,
+    we assume these list contain distinct items
+
+    @param      pygame      module pygame
+    @param      screen      screen (pygame surface)
+    @param      h           height of a line
+    @param      maxw        width of the screen
+    @param      seq1        list 1 (can be None)
+    @param      seq2        list 2 (cannot be None)
+    @param      diff        difference (object `SequenceMatcher <https://docs.python.org/3.5/library/difflib.html#sequencematcher-objects>`_)
+    @param      font        dictionary of fonts with keys ``'font'``, ``'font_small'``, ``'font_half'``
+    @param      bars        each item of sequence 2 can be associated to a width (in [0, 1])
+    @param      colors      dictionary of colors (see below)
+    @param      progress    draws the progress between two list
+    @param      prev_bars   previous width
+
+    Colors:
+
+    * black: no change
+    * blue: new
+    * red: deleted
+    * green: vert
+    * yellow: bars
+
+    When *progress* is not None, the picture is a kind of average
+    between the previous position and the new one. When a suggestion moves
+    from *p1* to *p2*, it defines a circle.
+    The result looks like this.
+
+    .. raw:: html
+
+        <video autoplay=" controls="" loop="" height="250">
+        <source src="http://www.xavierdupre.fr/enseignement/complements/diff.mp4" type="video/mp4" />
+        </video>
+
+    """
+    font = fonts.get('font', None)
+    font_small = fonts.get('font_small', None)
+    font_half = fonts.get('font_half', None)
+    if font is None:
+        raise ValueError("font cannot be None")
+    if font_small is None:
+        raise ValueError("font_small cannot be None")
+    if font_half is None:
+        raise ValueError("font_half cannot be None")
+    if seq2 is None:
+        raise ValueError("seq2 cannot be None")
+
+    if colors is None:
+        colors = {}
+    set_seq1 = {} if seq1 is None else set(seq1)
+    set_seq2 = set(seq2)
+    width = h // 3
+    color_bar = colors.get('yellow', (240, 240, 0))
+    pos = 0
+    if diff is not None:
+        if progress is None:
+            # just the diff
+            for opcode in diff.get_opcodes():
+                if opcode[0] == "delete":
+                    for i in range(opcode[1], opcode[2]):
+                        text = seq1[i]
+                        if text not in set_seq2:
+                            color = colors.get('red', (200, 0, 0))
+                            text = font_small.render(text, True, color)
+                            screen.blit(text, (10, h * pos + h // 6))
+                            pos += 1
+                        else:
+                            # we skip, it is going to be display by the other
+                            # part of the loop
+                            pass
+                elif opcode[0] == "equal":
+                    color = colors.get('black', (0, 0, 0))
+                    for i in range(opcode[3], opcode[4]):
+                        if bars is not None:
+                            y = h * pos + (h - width) // 2 + width
+                            pygame.draw.line(
+                                screen, color_bar, (0, y), (int(bars[i] * maxw), y), width)
+                        text = seq2[i]
+                        text = font.render(text, True, color)
+                        screen.blit(text, (10, h * pos))
+                        pos += 1
+                else:
+                    for i in range(opcode[3], opcode[4]):
+                        if bars is not None:
+                            y = h * pos + (h - width) // 2 + width
+                            pygame.draw.line(
+                                screen, color_bar, (0, y), (int(bars[i] * maxw), y), width)
+                        text = seq2[i]
+                        if text in set_seq1:
+                            color = colors.get('green', (0, 120, 0))
+                            text = font.render(text, True, color)
+                            screen.blit(text, (10, h * pos))
+                            pos += 1
+                        else:
+                            color = colors.get("blue", (0, 120, 120))
+                            text = font.render(text, True, color)
+                            screen.blit(text, (10, h * pos))
+                            pos += 1
+        else:
+            # animation
+            positions = []
+            for opcode in diff.get_opcodes():
+                if opcode[0] == "delete":
+                    for i in range(opcode[1], opcode[2]):
+                        row = (seq1[i], i, seq2.index(seq1[i]) if seq1[i] in seq2 else None,
+                               prev_bars[i] if prev_bars is not None else None)
+                        positions.append(row)
+                elif opcode[0] == "equal":
+                    for i in range(opcode[3], opcode[4]):
+                        row = (seq2[i], seq1.index(seq2[i]), i,
+                               bars[i] if bars is not None else None)
+                        positions.append(row)
+                else:
+                    for i in range(opcode[3], opcode[4]):
+                        row = (seq2[i], seq1.index(seq2[i]) if seq2[i] in seq1 else None, i,
+                               bars[i] if bars is not None else None)
+                        positions.append(row)
+            for text, p1, p2, bar in positions:
+                if p1 is None:
+                    # new
+                    x = maxw * (1 - progress)
+                    y = p2 * h
+                    color = colors.get('blue', (0, 120, 120))
+                elif p2 is None:
+                    # deleted
+                    x = maxw * progress
+                    y = p1 * h
+                    color = colors.get('green', (0, 120, 0))
+                else:
+                    # moved or equal
+                    if p1 == p2:
+                        x = 0.0
+                        y = p1 * h
+                    else:
+                        x = math.sin(progress * math.pi) * maxw / 2 * \
+                            abs(p2 - p1) / len(seq2)
+                        y = (p1 + p2) * h / 2 - (p2 - p1) * \
+                            h * math.cos(progress * math.pi) / 2
+                    color = colors.get('black', (0, 0, 0))
+
+                x = int(x)
+                y = int(y)
+                if bar is not None:
+                    y2 = y + (h - width) // 2 + width
+                    pygame.draw.line(screen, color_bar, (x, y2),
+                                     (x + int(bar * maxw), y2), width)
+                text = font.render(text, True, color)
+                screen.blit(text, (x, y))
+
+    else:
+        color = colors.get('black', (0, 0, 0))
+        for i in range(0, len(seq2)):
+            text = seq2[i]
+            text = font.render(text, True, color)
+            screen.blit(text, (10, h * pos))
+            pos += 1
