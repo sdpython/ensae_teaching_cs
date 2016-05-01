dde0667d369f034945945362ee2b3a72014899bd
dupre <xavier.dupre@ensae.fr>
2016-05-01 16:59:09 +0200

    update for pypdf

---
+++ _unittests/ut_modules/test_pdf.py
@@ -0,0 +1,107 @@
+"""
+@brief      test log(time=6s)
+"""
+
+
+import sys
+import os
+import unittest
+
+
+try:
+    import src
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+
+try:
+    import pyquickhelper as skip_
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import pyquickhelper as skip_
+
+try:
+    import pyPdf
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyPdf")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import pyPdf
+
+
+from pyquickhelper.loghelper import fLOG
+from src.ensae_teaching_cs.helpers.pypdf2_helper import pdf_read_content
+from pyPdf import PdfFileReader
+
+
+class TestModulesPdfReader(unittest.TestCase):
+
+    def test_read_pdf(self):
+        fLOG(__file__, self._testMethodName, OutputPrint=__name__ == "__main__")
+        pdffile = os.path.join(os.path.split(__file__)[
+                               0], "data", "1305.0445.pdf")
+        assert os.path.exists(pdffile)
+
+        with open(pdffile, "rb") as f:
+            input1 = PdfFileReader(f)
+            title = input1.getDocumentInfo().title
+            traw = input1.getDocumentInfo().title_raw
+            npage = input1.getNumPages()
+            fLOG("title", title, "*", traw)
+            fLOG("nb pages", npage)
+
+            page = input1.getPage(0)
+            cont = page.getContents()
+            fLOG("cont", cont)
+            for obj in page:
+                fLOG("obj", obj, "*", obj.title())
+            annots = page.raw_get("/Annots")
+            for a in annots:
+                fLOG("annot", a, dir(a))
+            for i in page.items():
+                fLOG("item", i)
+            text = page.extractText()
+            fLOG("text---", text)
+            assert " " in text
+            assert "\n" in text
+            if "algorithms: their inability" not in text:
+                raise Exception(text)
+
+    def test_read_function(self):
+        fLOG(__file__, self._testMethodName, OutputPrint=__name__ == "__main__")
+        pdffile = os.path.join(os.path.split(__file__)[
+                               0], "data", "1305.0445.pdf")
+        assert os.path.exists(pdffile)
+
+        text = pdf_read_content(pdffile)
+        assert " " in text
+        assert "\n" in text
+        if "algorithms:theirinability" not in text:
+            raise Exception(text)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/ensae_teaching_cs/helpers/pypdf2_helper.py
@@ -0,0 +1,42 @@
+"""
+@file
+@brief globals functions to manipulate PDF files
+"""
+
+from pyPdf import PdfFileReader
+
+
+def pdf_read_content(filename):
+    """
+    extracts the text from a PDF file
+
+    @param      filename            (str) filename
+    @return                         content (string)
+
+    The module was modified to introduce spaces. The method is not
+    very robust because it does not take into account the size of characters.
+    But the method PageObject.extractText can be modified to deal with
+    by introducing statistics or a better knowledge of PDF format.
+
+    The best way to introduce spaces and end of line is to study
+    the distribution of distances between consecutive characters assuming
+    we would fine a couple of modes:
+
+    * one for characters on the same line and from the same word,
+    * one for characters on the same line but separated by a space,
+    * one for characters on two different lines. This method
+      was not implemented yet.
+
+    If a line ends by "-", it is assumed a word was split. It is replaced by "---".
+
+    This function only works with  `sdpython/pyPdf <https://github.com/sdpython/pyPdf>`_.
+    """
+    texts = []
+    with open(filename, "rb") as f:
+        input1 = PdfFileReader(f)
+        npage = input1.getNumPages()
+        for ip in range(npage):
+            page = input1.getPage(ip)
+            text = page.extractText()
+            texts.append(text)
+    return "\n\n".join(texts)
