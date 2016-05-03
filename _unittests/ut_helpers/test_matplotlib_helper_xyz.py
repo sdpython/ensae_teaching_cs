5de5d2b968d6639b0ddd949acea04c50d0795b63
dupre <xavier.dupre@ensae.fr>
2016-05-03 22:36:02 +0200

    add 3D graphs

---
+++ _unittests/ut_helpers/test_matplotlib_helper_xyz.py
@@ -0,0 +1,160 @@
+"""
+@brief      test log(time=4s)
+
+"""
+import os
+import sys
+import unittest
+import random
+
+
+try:
+    import src
+    import pyquickhelper as skip_
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+    import pyquickhelper as skip_
+
+
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import get_temp_folder
+from src.ensae_teaching_cs.helpers.matplotlib_helper_xyz import scatter_xy_id, scatter_xyc, scatter_xyz
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+
+
+class TestMatplotlibHelperVizScatterPlots (unittest.TestCase):
+
+    backend = rcParams["backend"]
+
+    def generate_gauss(self, x, y, sigma, N=1000):
+        res = []
+        for i in range(N):
+            u = random.gauss(0, 1)
+            a = sigma * u + x
+            b = sigma * random.gauss(0, 1) + y + u
+            res.append((a, b))
+        return res
+
+    def test_viz_scatter_scatter_xyid(self):
+        fLOG(__file__, self._testMethodName,
+             OutputPrint=__name__ == "__main__")
+        rcParams["backend"] = "TkAgg"
+        fold = os.path.abspath(os.path.split(__file__)[0])
+        fold = os.path.join(fold, "temp_graph_xyz")
+        im = os.path.join(fold, self._testMethodName + ".png")
+        if not os.path.exists(fold):
+            os.mkdir(fold)
+        for img in [im]:
+            if os.path.exists(img):
+                os.remove(img)
+
+        nuage1 = self.generate_gauss(0, 0, 3)
+        nuage2 = self.generate_gauss(3, 4, 2)
+        nuage = [(a, b, 0) for a, b in nuage1] + [(a, b, 1) for a, b in nuage2]
+
+        matplotlib.use(matplotlib.get_backend(), warn=False, force=True)
+
+        fig, ax = scatter_xy_id(
+            nuage, title=self._testMethodName, legend={0: "c0", 1: "c1"})
+        fig.savefig(im)
+        assert os.path.exists(im)
+        rcParams["backend"] = TestMatplotlibHelperVizScatterPlots.backend
+        plt.close('all')
+        fLOG(plt.style.available)
+        # ['seaborn-bright', 'ggplot', 'seaborn-paper', 'seaborn-dark-palette', 'seaborn-dark',
+        # 'seaborn-ticks', 'seaborn-notebook', 'seaborn-white', 'seaborn-poster', 'classic',
+        # 'bmh', 'seaborn-talk', 'seaborn-pastel', 'seaborn-whitegrid', 'dark_background',
+        # 'seaborn-deep', 'grayscale', 'seaborn-muted', 'fivethirtyeight', 'seaborn-darkgrid', 'seaborn-colorblind']
+
+    def test_viz_scatter_scatter_xyc(self):
+        fLOG(__file__, self._testMethodName,
+             OutputPrint=__name__ == "__main__")
+        rcParams["backend"] = "TkAgg"
+        fold = os.path.abspath(os.path.split(__file__)[0])
+        fold = os.path.join(fold, "temp_graph_xyz")
+        im = os.path.join(fold, self._testMethodName + ".png")
+        if not os.path.exists(fold):
+            os.mkdir(fold)
+        for img in [im]:
+            if os.path.exists(img):
+                os.remove(img)
+
+        def f(a, b):
+            return (a ** 2 + b ** 2) ** 0.5
+
+        nuage1 = self.generate_gauss(0, 0, 3)
+        nuage2 = self.generate_gauss(3, 4, 2)
+        nuage = [(a, b, f(a, b)) for a, b in nuage1] + \
+            [(a, b, f(a, b)) for a, b in nuage2]
+
+        matplotlib.use(matplotlib.get_backend(), warn=False, force=True)
+
+        fig, ax = scatter_xyc(nuage, title=self._testMethodName)
+        fig.savefig(im)
+        assert os.path.exists(im)
+
+        fig, ax = scatter_xyc(
+            nuage, smooth=1, title=self._testMethodName + " - smooth 10")
+        fig.savefig(im.replace(".png", ".s10.png"))
+        assert os.path.exists(im)
+        rcParams["backend"] = TestMatplotlibHelperVizScatterPlots.backend
+        plt.close('all')
+
+    def test_viz_scatter_scatter_xyz(self):
+        fLOG(__file__, self._testMethodName,
+             OutputPrint=__name__ == "__main__")
+        rcParams["backend"] = "TkAgg"
+        fold = os.path.abspath(os.path.split(__file__)[0])
+        fold = os.path.join(fold, "temp_graph_xyz")
+        im = os.path.join(fold, self._testMethodName + ".png")
+        if not os.path.exists(fold):
+            os.mkdir(fold)
+        for img in [im]:
+            if os.path.exists(img):
+                os.remove(img)
+
+        def f(a, b):
+            return (a ** 2 + b ** 2) ** 0.5
+
+        nuage1 = self.generate_gauss(0, 0, 3)
+        nuage2 = self.generate_gauss(3, 4, 2)
+        nuage = [(a, b, f(a, b)) for a, b in nuage1] + \
+            [(a, b, f(a, b)) for a, b in nuage2]
+
+        matplotlib.use(matplotlib.get_backend(), warn=False, force=True)
+
+        fig, ax = scatter_xyz(nuage, title=self._testMethodName)
+        fig.savefig(im)
+        assert os.path.exists(im)
+
+        fig, ax = scatter_xyz(
+            nuage, smooth=2, title=self._testMethodName + " - smooth 10")
+        fig.savefig(im.replace(".png", ".s10.png"))
+        assert os.path.exists(im)
+        rcParams["backend"] = TestMatplotlibHelperVizScatterPlots.backend
+        plt.close('all')
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/ensae_teaching_cs/helpers/matplotlib_helper_xyz.py
@@ -0,0 +1,242 @@
+"""
+@file
+@brief      scatter plots
+"""
+
+import random
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.mlab import griddata
+import numpy
+from numpy.random import uniform, seed
+from scipy import ndimage, signal
+from .colorsdef import colors_definition
+
+
+def scatter_xy_id(xy_id, legend=None, ax=None, **options):
+    """
+    Creates a scatter plot with a different color for each zone id.
+    The function requires `matploblib <http://matplotlib.org/>`_.
+
+    @param      xy_id       list of 3-uple *(x, y, zone_id)*
+    @param      legend      dictionary {id: legend } or None if there is not any
+    @param      ax          existing graph to plot on (can be None)
+    @param      options     others options: xlabel, ylabel, title, marker, figsize (if ax is None)
+    @return                 fig, ax (fig is None if ax was sent to the function)
+
+    .. plot::
+        :include-source:
+
+        import random
+        def generate_gauss(x, y, sigma, N=1000):
+            res = []
+            for i in range(N):
+                u = random.gauss(0, 1)
+                a = sigma * u + x
+                b = sigma * random.gauss(0, 1) + y + u
+                res.append((a, b))
+            return res
+        nuage1 = generate_gauss(0, 0, 3)
+        nuage2 = generate_gauss(3, 4, 2)
+        nuage = [(a, b, 0) for a, b in nuage1] + [(a, b, 1) for a, b in nuage2]
+        import matplotlib.pyplot as plt
+        plt.style.use('ggplot')
+        from ensae_teaching_cs.helpers.matplotlib_helper_xyz import scatter_xy_id
+        fig, ax = scatter_xy_id(nuage, title="example with random observations",
+                                legend={0: "c0", 1: "c1"})
+        plt.show()
+
+    """
+    curves = {}
+    for x, y, zid in xy_id:
+        if zid not in curves:
+            curves[zid] = []
+        curves[zid].append((x, y))
+
+    marker = options.get('marker', 'o')
+    if ax is None:
+        fig, ax = plt.subplots(figsize=options.get('figsize', None))
+    else:
+        fig = None
+    color = 0
+    legs = []
+    for k in sorted(curves):
+        v = curves[k]
+        f, = ax.plot([_[0] for _ in v], [_[1] for _ in v],
+                     marker, color=colors_definition[color][1])
+        if legend != None:
+            legs.append((f, legend.get(k, "unknown:" + str(k))))
+        color += 1
+
+    if len(legs) > 0:
+        ax.legend([_[0] for _ in legs], [_[1] for _ in legs])
+
+    if "xlabel" in options:
+        ax.set_xlabel(options["xlabel"])
+    if "ylabel" in options:
+        ax.set_ylabel(options["ylabel"])
+    if "title" in options:
+        ax.set_title(options["title"])
+    return fig, ax
+
+
+def scatter_xyc(points, smooth=0, div=10, ax=None, **options):
+    """
+    Draw a 2D graph (X,Y, color), the color is chosen based on a value f(x,y)
+    The function requires `matploblib <http://matplotlib.org/>`_
+    and `SciPy <http://www.scipy.org/>`_
+
+    @param      points      (x,y, z=f(x,y) )
+    @param      smooth      applies n times a smoothing I * M (convolutional)
+    @param      div         number of divisions for axis
+    @param      options     others options: xlabel, ylabel, title, figsize (if ax is None)
+    @return                 fig, ax (fig is None if ax was sent to the function)
+
+    .. plot::
+        :include-source:
+
+        import random
+        def generate_gauss(x, y, sigma, N=1000):
+            res = []
+            for i in range(N):
+                u = random.gauss(0, 1)
+                a = sigma * u + x
+                b = sigma * random.gauss(0, 1) + y + u
+                res.append((a, b))
+            return res
+        def f(a, b):
+            return (a ** 2 + b ** 2) ** 0.5
+        nuage1 = generate_gauss(0, 0, 3)
+        nuage2 = generate_gauss(3, 4, 2)
+        nuage = [(a, b, f(a, b)) for a, b in nuage1] + [(a, b, f(a, b)) for a, b in nuage2]
+        import matplotlib.pyplot as plt
+        plt.style.use('ggplot')
+        from ensae_teaching_cs.helpers.matplotlib_helper_xyz import scatter_xyc
+        fig, ax = scatter_xyc(nuage, title="example with random observations")
+        plt.show()
+
+    """
+    if ax is None:
+        fig, ax = plt.subplots(figsize=options.get('figsize', None))
+    else:
+        fig = None
+
+    x = [_[0] for _ in points]
+    y = [_[1] for _ in points]
+    z = [_[2] for _ in points]
+
+    bound = [min(x), min(y), max(x), max(y)]
+    xi = numpy.linspace(bound[0], bound[2], div)
+    yi = numpy.linspace(bound[1], bound[3], div)
+    dx = xi[1:] - xi[0:-1]
+    dy = yi[1:] - yi[0:-1]
+
+    zi = griddata(x, y, z, xi, yi, interp='nn')
+
+    if smooth > 0:
+        w = 1. / 9
+        iii = numpy.array([[w, w, w], [w, w, w], [w, w, w]])
+        for i in range(smooth):
+            zi = ndimage.convolve(zi, iii)
+
+    CS = plt.contour(xi, yi, zi, 15, linewidths=0.5, colors='k')
+    CS = plt.contourf(xi, yi, zi, 15, cmap=plt.cm.rainbow,
+                      vmax=abs(zi).max(), vmin=abs(zi).min())
+    plt.colorbar(ax=ax)
+    ax.scatter(x, y, c='b', s=5, zorder=10)
+    ax.set_xlim(bound[0], bound[2])
+    ax.set_ylim(bound[1], bound[3])
+
+    if "xlabel" in options:
+        ax.set_xlabel(options["xlabel"])
+    if "ylabel" in options:
+        ax.set_ylabel(options["ylabel"])
+    if "title" in options:
+        ax.set_title(options["title"])
+    return fig, ax
+
+
+def scatter_xyz(points, smooth=0, div=100, ax=None, **options):
+    """
+    Draw a 3D graph (X,Y, Z)
+    The function requires `matploblib <http://matplotlib.org/>`_
+    and `SciPy <http://www.scipy.org/>`_
+
+    @param      points      (x,y, z=f(x,y) )
+    @param      div         number of divisions for axis
+    @param      smooth      applies n times a smoothing I * M (convolutional)
+    @param      ax          existing graph to plot on (can be None)
+    @param      options     others options: xlabel, ylabel, zlabel, title, elev, angle, figsize (if ax is None)
+                                - elev, angle: see `view_init <http://matplotlib.org/mpl_toolkits/mplot3d/api.html>`_)
+    @return                 fig, ax (fig is None if ax was sent to the function)
+
+    If *ax is None*, axes are created like::
+
+        fig = plt.figure(figsize=options.get('figsize', None))
+        ax = fig.gca(projection='3d')
+
+    .. plot::
+        :include-source:
+
+        import random
+        def generate_gauss(x, y, sigma, N=1000):
+            res = []
+            for i in range(N):
+                u = random.gauss(0, 1)
+                a = sigma * u + x
+                b = sigma * random.gauss(0, 1) + y + u
+                res.append((a, b))
+            return res
+        def f(a, b):
+            return (a ** 2 + b ** 2) ** 0.5
+        nuage1 = generate_gauss(0, 0, 3)
+        nuage2 = generate_gauss(3, 4, 2)
+        nuage = [(a, b, f(a, b)) for a, b in nuage1] + [(a, b, f(a, b)) for a, b in nuage2]
+        import matplotlib.pyplot as plt
+        plt.style.use('ggplot')
+        from ensae_teaching_cs.helpers.matplotlib_helper_xyz import scatter_xyz
+        fig, ax = scatter_xyz(nuage, title="example with random observations")
+        plt.show()
+
+    """
+    x = [_[0] for _ in points]
+    y = [_[1] for _ in points]
+    z = [_[2] for _ in points]
+
+    bound = [min(x), min(y), max(x), max(y)]
+    xi = numpy.linspace(bound[0], bound[2], div)
+    yi = numpy.linspace(bound[1], bound[3], div)
+    dx = xi[1:] - xi[0:-1]
+    dy = yi[1:] - yi[0:-1]
+
+    zi = griddata(x, y, z, xi, yi, interp='nn')
+    if smooth > 0:
+        w = 1. / 9
+        iii = numpy.array([[w, w, w], [w, w, w], [w, w, w]])
+        for i in range(smooth):
+            zi = ndimage.convolve(zi, iii)
+
+    if ax is None:
+        fig = plt.figure(figsize=options.get('figsize', None))
+        ax = fig.gca(projection='3d')
+    else:
+        fig = None
+
+    elev = options.get("elev", 50)
+    angle = options.get("angle", 45)
+    ax.view_init(elev, angle)
+
+    X = numpy.row_stack(tuple([xi] * len(yi)))
+    Y = numpy.column_stack(tuple([yi] * len(xi)))
+
+    surf = ax.plot_surface(X, Y, zi, cmap='autumn', cstride=5, rstride=5)
+
+    if "xlabel" in options:
+        ax.set_xlabel(options["xlabel"])
+    if "ylabel" in options:
+        ax.set_ylabel(options["ylabel"])
+    if "zlabel" in options:
+        ax.set_ylabel(options["zlabel"])
+    if "title" in options:
+        ax.set_title(options["title"])
+    return fig, ax
