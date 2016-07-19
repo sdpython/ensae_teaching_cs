cc9e78ae3d5f9f4b525ff60c6b5456ef45e18975
dupre <xavier.dupre@ensae.fr>
2016-07-20 00:42:46 +0200

    fix conf.py

--- _doc/sphinxdoc/source/conf_base.py
+++ _doc/sphinxdoc/source/conf_base.py
@@ -67,31 +67,33 @@ set_sphinx_variables(__file__, "Python dans tous ses états", "Xavier Dupré",
 # import pymmails
 # import pyrsslocal
 
-imgmath_latex_preamble += """\n\\newcommand{\\girafedec}[3]{ \\begin{array}{ccccc} #1 &=& #2 &+& #3 \\\\ a' &=& a &-& o  \\end{array}}
-            \\newcommand{\\vecteur}[2]{\\pa{#1,\\dots,#2}}
-            \\newcommand{\\N}[0]{\\mathbb{N}}
-            \\newcommand{\\indicatrice}[1]{\\mathbf{1\\!\\!1}_{\\acc{#1}}}
-            \\usepackage[all]{xy}
-            \\newcommand{\\infegal}[0]{\\leqslant}
-            \\newcommand{\\supegal}[0]{\\geqslant}
-            \\newcommand{\\ensemble}[2]{\\acc{#1,\\dots,#2}}
-            \\newcommand{\\fleche}[1]{\\overrightarrow{ #1 }}
-            \\newcommand{\\intervalle}[2]{\\left\\{#1,\\cdots,#2\\right\\}}
-            \\newcommand{\\loinormale}[2]{{\\cal N}\\pa{#1,#2}}
-            \\newcommand{\\independant}[0]{\\;\\makebox[3ex]{\\makebox[0ex]{\\rule[-0.2ex]{3ex}{.1ex}}\\!\\!\\!\\!\\makebox[.5ex][l]{\\rule[-.2ex]{.1ex}{2ex}}\\makebox[.5ex][l]{\\rule[-.2ex]{.1ex}{2ex}}} \\,\\,}
-            \\newcommand{\\esp}{\\mathbb{E}}
-            \\newcommand{\\pr}[1]{\\mathbb{P}\\pa{#1}}
-            \\newcommand{\\loi}[0]{{\\cal L}}
-            \\newcommand{\\vecteurno}[2]{#1,\\dots,#2}
-            \\newcommand{\\norm}[1]{\\left\\Vert#1\\right\\Vert}
-            \\newcommand{\\dans}[0]{\\rightarrow}
-            \\newcommand{\\partialfrac}[2]{\\frac{\\partial #1}{\\partial #2}}
-            \\newcommand{\\partialdfrac}[2]{\\dfrac{\\partial #1}{\\partial #2}}
-            \\newcommand{\\loimultinomiale}[1]{{\\cal M}\\pa{#1}}
-            \\newcommand{\\trace}[1]{tr\\pa{#1}}
-            \\newcommand{\\abs}[1]{\\left|#1\\right|}
-            """
+custom_preamble += """\n\\newcommand{\\girafedec}[3]{ \\begin{array}{ccccc} #1 &=& #2 &+& #3 \\\\ a' &=& a &-& o  \\end{array}}
+\\newcommand{\\vecteur}[2]{\\pa{#1,\\dots,#2}}
+\\newcommand{\\N}[0]{\\mathbb{N}}
+\\newcommand{\\indicatrice}[1]{\\mathbf{1\\!\\!1}_{\\acc{#1}}}
+\\usepackage[all]{xy}
+\\newcommand{\\infegal}[0]{\\leqslant}
+\\newcommand{\\supegal}[0]{\\geqslant}
+\\newcommand{\\ensemble}[2]{\\acc{#1,\\dots,#2}}
+\\newcommand{\\fleche}[1]{\\overrightarrow{ #1 }}
+\\newcommand{\\intervalle}[2]{\\left\\{#1,\\cdots,#2\\right\\}}
+\\newcommand{\\loinormale}[2]{{\\cal N}\\pa{#1,#2}}
+\\newcommand{\\independant}[0]{\\;\\makebox[3ex]{\\makebox[0ex]{\\rule[-0.2ex]{3ex}{.1ex}}\\!\\!\\!\\!\\makebox[.5ex][l]{\\rule[-.2ex]{.1ex}{2ex}}\\makebox[.5ex][l]{\\rule[-.2ex]{.1ex}{2ex}}} \\,\\,}
+\\newcommand{\\esp}{\\mathbb{E}}
+\\newcommand{\\pr}[1]{\\mathbb{P}\\pa{#1}}
+\\newcommand{\\loi}[0]{{\\cal L}}
+\\newcommand{\\vecteurno}[2]{#1,\\dots,#2}
+\\newcommand{\\norm}[1]{\\left\\Vert#1\\right\\Vert}
+\\newcommand{\\dans}[0]{\\rightarrow}
+\\newcommand{\\partialfrac}[2]{\\frac{\\partial #1}{\\partial #2}}
+\\newcommand{\\partialdfrac}[2]{\\dfrac{\\partial #1}{\\partial #2}}
+\\newcommand{\\loimultinomiale}[1]{{\\cal M}\\pa{#1}}
+\\newcommand{\\trace}[1]{tr\\pa{#1}}
+\\newcommand{\\abs}[1]{\\left|#1\\right|}
+"""        
 #\\usepackage{eepic}
+imgmath_latex_preamble += custom_preamble
+latex_elements['preamble'] += custom_preamble
 
 project_var_name_t = "ENSAE<br />Xavier Dupré"
 project_var_name = "ensae_teaching_cs"
