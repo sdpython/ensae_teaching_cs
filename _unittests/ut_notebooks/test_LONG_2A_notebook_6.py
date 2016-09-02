78a4ae7b25dbda11211834c81fc41cbd10b1864f
dupre <xavier.dupre@ensae.fr>
2016-09-03 00:04:15 +0200

    rename unit test

--- _unittests/ut_notebooks/test_LONG_2A_notebook_6.py
+++ _unittests/ut_notebooks/test_LONG_2A_notebook_6.py
@@ -37,17 +37,21 @@ except ImportError:
     import pyquickhelper as skip_
 
 from pyquickhelper.loghelper import fLOG
-from pyquickhelper.pycode import get_temp_folder
-from src.ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, execute_notebooks, unittest_raise_exception_notebook
+from pyquickhelper.pycode import get_temp_folder, add_missing_development_version
 
 
 class TestNotebookRunner2a_6 (unittest.TestCase):
+
+    def setUp(self):
+        add_missing_development_version(["pymyinstall", "pyensae", "pymmails"],
+                                        __file__, hide=True)
 
     def test_notebook_runner(self):
         fLOG(
             __file__,
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
+        from src.ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, execute_notebooks, unittest_raise_exception_notebook
         temp = get_temp_folder(__file__, "temp_notebook2a_6")
         keepnote = ls_notebooks("td2a")
         assert len(keepnote) > 0
