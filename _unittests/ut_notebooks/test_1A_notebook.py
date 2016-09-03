c47279b32af2fac33ef50ed9cad454896951e0f1
dupre <xavier.dupre@ensae.fr>
2016-09-03 02:46:31 +0200

    update notrebooks

--- _unittests/ut_notebooks/test_1A_notebook.py
+++ _unittests/ut_notebooks/test_1A_notebook.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 """
-@brief      test log(time=60s)
+@brief      test log(time=50s)
 """
 
 import sys
@@ -39,17 +39,21 @@ except ImportError:
 
 
 from pyquickhelper.loghelper import fLOG
-from pyquickhelper.pycode import get_temp_folder
-from src.ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, execute_notebooks, clean_function_1a, unittest_raise_exception_notebook
+from pyquickhelper.pycode import get_temp_folder, add_missing_development_version
 
 
 class TestNotebookRunner1a_ (unittest.TestCase):
+
+    def setUp(self):
+        add_missing_development_version(["pymyinstall", "pyensae", "pymmails"],
+                                        __file__, hide=True)
 
     def test_notebook_runner_1a(self):
         fLOG(
             __file__,
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
+        from src.ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, execute_notebooks, clean_function_1a, unittest_raise_exception_notebook
         temp = get_temp_folder(__file__, "temp_notebook1a_")
         keepnote = ls_notebooks("1a")
         assert len(keepnote) > 0
