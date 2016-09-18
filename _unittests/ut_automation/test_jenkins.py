ad2519a21e17be642d37c57f0c8f6eaa6eeb1ad2
dupre <xavier.dupre@ensae.fr>
2016-09-18 17:37:49 +0200

    pep8 + unittest

--- _unittests/ut_automation/test_jenkins.py
+++ _unittests/ut_automation/test_jenkins.py
@@ -150,7 +150,7 @@ class TestJenkins(unittest.TestCase):
             OutputPrint=__name__ == "__main__")
 
         modules = default_jenkins_jobs(".*yml.*", ".*update.*")
-        self.assertEqual(len(modules), 13)
+        self.assertEqual(len(modules), 14)
         modules = default_jenkins_jobs(".*27.*", ".*update.*")
         self.assertEqual(len(modules), 0)
         modules = default_jenkins_jobs(".*27.*")
