e4b20fdd647e0d6a32beecac800ee28cb21a5c9c
dupre <xavier.dupre@ensae.fr>
2016-09-26 23:50:13 +0200

    update unit test

--- _unittests/ut_notebooks/test_LONG_2A_notebook_2_correction.py
+++ _unittests/ut_notebooks/test_LONG_2A_notebook_2_correction.py
@@ -5,6 +5,7 @@
 import sys
 import os
 import unittest
+import shutil
 
 
 try:
@@ -37,17 +38,22 @@ except ImportError:
     import pyquickhelper as skip_
 
 from pyquickhelper.loghelper import fLOG
-from pyquickhelper.pycode import get_temp_folder
-from src.ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, execute_notebooks, unittest_raise_exception_notebook
+from pyquickhelper.pycode import get_temp_folder, add_missing_development_version
 
 
 class TestNotebookRunner2a_2_correction (unittest.TestCase):
+
+    def setUp(self):
+        add_missing_development_version(["pymyinstall", "pyensae", "pymmails", "jyquickhelper"],
+                                        __file__, hide=True)
 
     def test_notebook_runner(self):
         fLOG(
             __file__,
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
+        from src.ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, execute_notebooks, unittest_raise_exception_notebook
+        from src.ensae_teaching_cs.data import simple_database
         temp = get_temp_folder(__file__, "temp_notebook2a_2_corection")
         keepnote = ls_notebooks("td2a")
         assert len(keepnote) > 0
