605fbc61811e77f76e9441ca7e06be43aaf9a5d6
dupre <xavier.dupre@ensae.fr>
2016-09-30 02:13:54 +0200

    update unit test

--- _unittests/ut_notebooks/test_2A_notebook_2_enonce.py
+++ _unittests/ut_notebooks/test_2A_notebook_2_enonce.py
@@ -37,6 +37,22 @@ except ImportError:
         sys.path.append(path)
     import pyquickhelper as skip_
 
+try:
+    import jyquickhelper as skip__
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "jyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import jyquickhelper as skip__
+
 from pyquickhelper.loghelper import fLOG
 from pyquickhelper.pycode import get_temp_folder, is_travis_or_appveyor, add_missing_development_version
 
