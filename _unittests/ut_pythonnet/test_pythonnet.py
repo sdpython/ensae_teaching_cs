3e9efb51a28d8565a688bd33682338fb53161512
xavier dupré <xavier.dupre@gmail.com>
2017-01-05 21:54:00 +0000

    fix two unit tests

--- _unittests/ut_pythonnet/test_pythonnet.py
+++ _unittests/ut_pythonnet/test_pythonnet.py
@@ -74,6 +74,8 @@ class TestPythonnet(unittest.TestCase):
                     # Audia device)
                     if os.environ["USERNAME"] == "ensaestudent" or \
                        os.environ["USERNAME"] == "vsxavierdupre" or \
+                       os.environ["USERNAME"] == "vsxavierdupre" or \
+                       "DOUZE2016" in os.environ["USERDOMAIN"] or \
                        os.environ["USERNAME"] == "appveyor" or \
                        "paris" in os.environ["COMPUTERNAME"].lower() or \
                        os.environ["USERNAME"].endswith("$"):  # anonymous Jenkins configuration
