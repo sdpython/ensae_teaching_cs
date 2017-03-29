e798a0dd2df527d546038e3895aee22e76406dc9
xavier dupré <xavier.dupre@gmail.com>
2017-03-29 21:02:42 +0200

    update unit test

--- _unittests/ut_notebooks/test_LONG_1A_notebook_8_correction.py
+++ _unittests/ut_notebooks/test_LONG_1A_notebook_8_correction.py
@@ -39,24 +39,31 @@ except ImportError:
         sys.path.append(path)
     import pyquickhelper as skip_
 
-from pyquickhelper.loghelper import fLOG
-from pyquickhelper.pycode import get_temp_folder
-from src.ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, execute_notebooks, clean_function_1a, unittest_raise_exception_notebook
+from pyquickhelper.loghelper import fLOG, CustomLog
+from pyquickhelper.pycode import get_temp_folder, add_missing_development_version
 
 
 class TestNotebookRunner1a_correction (unittest.TestCase):
+
+    def setUp(self):
+        fLOG("add missing dependencies", OutputPrint=__name__ == "__main__")
+        add_missing_development_version(["pymyinstall", "pyensae", "pymmails", "jyquickhelper"],
+                                        __file__, hide=True)
 
     def test_notebook_runner_correction_8(self):
         fLOG(
             __file__,
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
+        from src.ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, execute_notebooks, clean_function_1a
+        from src.ensae_teaching_cs.automation.notebook_test_helper import unittest_raise_exception_notebook
         temp = get_temp_folder(__file__, "temp_notebook1a_correction_8")
         keepnote = ls_notebooks("td1a")
         assert len(keepnote) > 0
+        clog = CustomLog(temp)
         res = execute_notebooks(temp, keepnote,
                                 lambda i, n: "correction_session8." in n,
-                                fLOG=fLOG,
+                                fLOG=clog,
                                 clean_function=clean_function_1a)
         unittest_raise_exception_notebook(res, fLOG)
 
