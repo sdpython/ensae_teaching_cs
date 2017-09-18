d88526871f1b185e8ed81bf04d1617d787ed2060
xavier dupré <xavier.dupre@gmail.com>
2017-09-18 23:58:34 +0200

    fix unit tests on algos

--- _unittests/ut_dnotebooks/test_SKIP_2A_notebook_6.py
+++ _unittests/ut_dnotebooks/test_SKIP_2A_notebook_6.py
@@ -5,6 +5,7 @@
 import sys
 import os
 import unittest
+import shutil
 
 
 try:
@@ -55,6 +56,12 @@ class TestNotebookRunner2a_6 (unittest.TestCase):
         from src.ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, execute_notebooks
         temp = get_temp_folder(__file__, "temp_notebook2a_6")
         keepnote = ls_notebooks("td2a_algo")
+        fold = os.path.dirname(keepnote[0])
+        for png in os.listdir(fold):
+            if ".png" not in png:
+                continue
+            fLOG("copy", png)
+            shutil.copy(os.path.join(fold, png), temp)
         execute_notebooks(temp, keepnote, lambda i, n: "_6" in n,
                           fLOG=fLOG, dump=src.ensae_teaching_cs)
 
