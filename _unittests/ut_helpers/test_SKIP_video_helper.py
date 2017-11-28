0f69f9d1e168e56dea8b61179435307073399513
xavier dupré <xavier.dupre@gmail.com>
2017-11-28 15:04:48 +0100

    attempt to fix an import

--- _unittests/ut_helpers/test_SKIP_video_helper.py
+++ _unittests/ut_helpers/test_SKIP_video_helper.py
@@ -2,6 +2,7 @@
 @brief      test log(time=10s)
 
 """
+import re
 import os
 import sys
 import warnings
@@ -52,6 +53,7 @@ class TestVideoHelper(ExtTestCase):
         This test does not work under a virtual environment.
         """
         fLOG(__file__, self._testMethodName, OutputPrint=__name__ == "__main__")
+        assert re is not None
 
         if is_travis_or_appveyor() == "travis":
             warnings.warn("cv2 is not available")
