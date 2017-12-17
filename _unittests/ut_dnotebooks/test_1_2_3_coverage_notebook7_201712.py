04f04659ff433d9ff6e75c84d22405d74872c67e
xavier dupré <xavier.dupre@gmail.com>
2017-12-17 11:52:21 +0100

    extend notebook coverage

---
+++ _unittests/ut_dnotebooks/test_1_2_3_coverage_notebook7_201712.py
@@ -0,0 +1,81 @@
+#-*- coding: utf-8 -*-
+"""
+@brief      test log(time=13s)
+"""
+
+import sys
+import os
+import unittest
+
+
+try:
+    import src.ensae_teaching_cs as skip__
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src.ensae_teaching_cs as skip__
+
+try:
+    import pyquickhelper as skip_
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import pyquickhelper as skip_
+
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import add_missing_development_version
+
+
+class TestNotebook1237Coverage201712(unittest.TestCase):
+
+    def setUp(self):
+        add_missing_development_version(["pymyinstall", "pyensae", "pymmails", "jyquickhelper"],
+                                        __file__, hide=True)
+
+    def a_test_notebook_runner(self, name, folder, valid=None, copy_files=None, modules=None):
+        from src.ensae_teaching_cs.automation.notebook_test_helper import a_test_notebook_runner
+        return a_test_notebook_runner(__file__, name, folder, valid=valid,
+                                      copy_files=copy_files, modules=modules, fLOG=fLOG)
+
+    def test_notebook_dimensions_reduction(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.a_test_notebook_runner("dimensions_reduction", "notebook_eleves/2017-2018")
+
+    def test_notebook_git_notebook(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.a_test_notebook_runner("git_notebook", "2a")
+
+    def test_notebook_td2a_bigdata_memory(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.a_test_notebook_runner("td2a_bigdata_memory", "td2a")
+
+
+if __name__ == "__main__":
+    unittest.main()
