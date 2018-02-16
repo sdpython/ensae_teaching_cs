bf3d9e5b860f752b303cd83bc0a05fcd638bbfa4
xavier dupré <xavier.dupre@gmail.com>
2018-02-16 21:00:02 +0100

    moved files to papierstat

--- src/ensae_teaching_cs/ml/__init__.py
+++ src/ensae_teaching_cs/ml/__init__.py
@@ -3,8 +3,4 @@
 @brief Shortcuts for ML
 """
 
-from .sklearn_parameters import SkException, SkLearnParameters
-from .sklearn_base_learner import SkBaseLearner
-from .sklearn_base_regressor import SkBaseRegressor
-from .sklearn_base_classifier import SkBaseClassifier
 from .categories_to_integers import CategoriesToIntegers
