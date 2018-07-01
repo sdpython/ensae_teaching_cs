0f66b6960617972361baff235296f9608fc36ab3
xavier dupré <xavier.dupre@gmail.com>
2018-07-02 00:16:43 +0200

    fix one unit test

--- _unittests/ut_automation/test_jenkins.py
+++ _unittests/ut_automation/test_jenkins.py
@@ -64,7 +64,7 @@ class TestJenkins(unittest.TestCase):
             OutputPrint=__name__ == "__main__")
 
         modules = default_jenkins_jobs(".*yml.*", ".*update.*")
-        self.assertEqual(len(modules), 29)
+        self.assertEqual(len(modules), 30)
         modules = default_jenkins_jobs(".*27.*", ".*update.*")
         self.assertEqual(len(modules), 0)
         modules = default_jenkins_jobs(".*27.*")
