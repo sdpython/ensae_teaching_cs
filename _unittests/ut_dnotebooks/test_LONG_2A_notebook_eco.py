b42a99fd28b8665ac5e7aa37946f2d5217fa2cd4
xavier dupré <xavier.dupre@gmail.com>
2019-01-08 13:29:53 +0100

    split unit tests about notebooks

--- _unittests/ut_dnotebooks/test_LONG_2A_notebook_eco.py
+++ _unittests/ut_dnotebooks/test_LONG_2A_notebook_eco.py
@@ -24,13 +24,13 @@ except ImportError:
     import src
 
 
-class TestNotebookRunner2aEco(unittest.TestCase):
+class TestLONGNotebookRunner2aEco(unittest.TestCase):
 
     def setUp(self):
         add_missing_development_version(["pymyinstall", "pyensae", "pymmails", "jyquickhelper"],
                                         __file__, hide=True)
 
-    def test_notebook_runner_2a_eco(self):
+    def test_notebook_runner_2a_eco_nlp_long(self):
         fLOG(
             __file__,
             self._testMethodName,
@@ -42,23 +42,14 @@ class TestNotebookRunner2aEco(unittest.TestCase):
 
         from src.ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, execute_notebooks, clean_function_1a
         from src.ensae_teaching_cs.data import simple_database
-        temp = get_temp_folder(__file__, "temp_notebook2a_eco")
+        temp = get_temp_folder(__file__, "temp_notebook2a_eco_nlp_long")
         keepnote = ls_notebooks("td2a_eco")
         shutil.copy(simple_database(), temp)
 
         def filter(i, n):
-            if "SNCF" in n:
-                return False
-            if "Scraping" in n:
-                return False
-            if "2.ipynb" in n:
-                return False
-            if "flask" in n.lower():
-                # flask from a notebook does not work
-                return False
             if "td2a_TD5_Traitement_automatique_des_langues_en_Python" in n:
-                return False
-            return True
+                return True
+            return False
 
         execute_notebooks(temp, keepnote,
                           filter,
