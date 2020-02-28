cf07ace64af644f95bbf39f9024a927e5e73ac3a
xavier dupré <xavier.dupre@gmail.com>
2020-02-29 00:19:01 +0100

    Update test_sudoku.py

--- _unittests/ut_special/test_sudoku.py
+++ _unittests/ut_special/test_sudoku.py
@@ -3,12 +3,13 @@
 
 """
 import unittest
+from pyquickhelper.pycode import ExtTestCase
 from ensae_teaching_cs.special import resolution_sudoku, sudoku2str
 
 
-class TestSudoku(unittest.TestCase):
+class TestSudoku(ExtTestCase):
 
-    def test_sudolu(self):
+    def test_sudoku(self):
         s = [[0, 0, 0, 3, 0, 0, 8, 0, 0],
              [0, 0, 7, 9, 0, 8, 0, 0, 5],
              [0, 0, 0, 2, 0, 4, 1, 0, 0],
@@ -20,7 +21,7 @@ class TestSudoku(unittest.TestCase):
              [0, 0, 0, 7, 0, 0, 0, 0, 0]]
 
         ps = sudoku2str(s)
-        assert ps is not None
+        self.assertNotEmpty(ps)
 
         resolution_sudoku(s)
 
@@ -30,7 +31,32 @@ class TestSudoku(unittest.TestCase):
             self.assertEqual(sum(s[i]), sum(range(1, 10)))
 
         ps = sudoku2str(s)
-        assert ps is not None
+        self.assertNotEmpty(ps)
+
+    def test_sudoku_cplx(self):
+        s = [[0, 0, 4, 6, 0, 0, 0, 7, 0],
+             [0, 7, 0, 0, 5, 0, 0, 0, 0],
+             [0, 0, 6, 0, 8, 0, 0, 0, 4],
+             [7, 0, 3, 0, 4, 0, 1, 0, 0],
+             [0, 4, 0, 0, 0, 2, 0, 0, 5],
+             [0, 6, 0, 0, 0, 5, 0, 0, 0],
+             [8, 0, 0, 0, 1, 0, 0, 0, 7],
+             [0, 0, 7, 0, 0, 0, 0, 2, 0],
+             [0, 0, 0, 0, 0, 9, 6, 0, 0],
+             ]
+
+        ps = sudoku2str(s)
+        self.assertNotEmpty(ps)
+
+        resolution_sudoku(s)
+
+        self.assertEqual(len(s), 9)
+        for i in range(0, 9):
+            self.assertEqual(len(s[i]), 9)
+            self.assertEqual(sum(s[i]), sum(range(1, 10)))
+
+        ps = sudoku2str(s)
+        self.assertNotEmpty(ps)
 
 
 if __name__ == "__main__":
