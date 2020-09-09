515121d5bc5e75e6074a60423ad68e992e1d2561
xavier dupré <xavier.dupre@gmail.com>
2020-09-09 15:35:22 +0200

    update notebooks

---
+++ _unittests/ut_dnotebooks/test_1A_notebook_home.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+@brief      test log(time=12s)
+"""
+import unittest
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import get_temp_folder, add_missing_development_version
+import ensae_teaching_cs
+
+
+class TestNotebookRunner1a_home(unittest.TestCase):
+
+    def setUp(self):
+        add_missing_development_version(["pymyinstall", "pyensae", "pymmails", "jyquickhelper", "mlstatpy"],
+                                        __file__, hide=True)
+
+    def test_notebook_runner_enonce_home(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+        temp = get_temp_folder(__file__, "temp_notebook1a_home")
+        from ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, execute_notebooks
+        keepnote = ls_notebooks("td1a_home")
+
+        execute_notebooks(temp, keepnote, filter, fLOG=fLOG,
+                          dump=ensae_teaching_cs)
+
+
+if __name__ == "__main__":
+    unittest.main()
