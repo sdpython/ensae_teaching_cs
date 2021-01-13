12cd20aa74f27a359e48dceeb4070e9ec66f9528
xavier dupré <xavier.dupre@gmail.com>
2021-01-13 16:57:17 +0100

    update notebook showing cython

--- _unittests/ut_dnotebooks/test_1A_notebook_soft_cython.py
+++ _unittests/ut_dnotebooks/test_1A_notebook_soft_cython.py
@@ -8,24 +8,24 @@ from pyquickhelper.pycode import get_temp_folder, add_missing_development_versio
 import ensae_teaching_cs
 
 
-class TestNotebookRunner1a_soft_sql(unittest.TestCase):
+class TestNotebookRunner1a_soft_cython(unittest.TestCase):
 
     def setUp(self):
         add_missing_development_version(["pymyinstall", "pyensae", "pymmails", "jyquickhelper", "mlstatpy"],
                                         __file__, hide=True)
 
-    def test_notebook_runner_soft_sql(self):
+    def test_notebook_runner_soft_cython(self):
         fLOG(
             __file__,
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
-        temp = get_temp_folder(__file__, "temp_notebook1a_soft_sql")
+        temp = get_temp_folder(__file__, "temp_notebook1a_soft_cython")
         from ensae_teaching_cs.automation.notebook_test_helper import ls_notebooks, execute_notebooks, clean_function_1a
         keepnote = ls_notebooks("td1a_soft")
         for n in keepnote:
             fLOG(n)
         execute_notebooks(temp, keepnote,
-                          lambda i, n: "csharp" not in n and "cython" not in n,
+                          lambda i, n: "csharp" not in n and "cython" in n,
                           fLOG=fLOG,
                           clean_function=clean_function_1a,
                           dump=ensae_teaching_cs)
