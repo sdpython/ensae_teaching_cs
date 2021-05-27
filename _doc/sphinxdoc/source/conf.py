b7393ed6eb30099e11ceea6ca5905c0fb4746672
xavier dupré <xavier.dupre@gmail.com>
2021-05-27 02:26:50 +0200

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
+from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 import ensae_teaching_cs
 import pydata_sphinx_theme
 
