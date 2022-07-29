0a014ffd69e622b59ef073024c5e3ccefdccee54
xavier dupré <xavier.dupre@gmail.com>
2022-07-29 23:19:48 +0200

    Update test_jenkins.py

--- _unittests/ut_automation/test_jenkins.py
+++ _unittests/ut_automation/test_jenkins.py
@@ -4,7 +4,7 @@
 """
 import sys
 import unittest
-from pyquickhelper.loghelper import fLOG
+from pyquickhelper.loghelper import noLOG
 from pyquickhelper.pycode import ExtTestCase
 from pyquickhelper.jenkinshelper import JenkinsExt
 from ensae_teaching_cs.automation.jenkins_helper import (
@@ -14,11 +14,6 @@ from ensae_teaching_cs.automation.jenkins_helper import (
 class TestJenkins(ExtTestCase):
 
     def test_jenkins_local(self):
-        fLOG(
-            __file__,
-            self._testMethodName,
-            OutputPrint=__name__ == "__main__")
-
         engines = dict(anaconda2="C:\\Anaconda2", anaconda3="C:\\Anaconda3",
                        winpython="C:\\WinPython-64bit-3.4.2.3\\python-3.4.2.amd64",
                        default="c:\\PythonXX_x64", py27="c:\\Python27", py35="c:\\Python35_x64",
@@ -30,17 +25,12 @@ class TestJenkins(ExtTestCase):
         engines["Python" + vers] = "py" + vers
 
         js = JenkinsExt('http://machine:8080/', "user",
-                        "password", mock=True, engines=engines, fLOG=fLOG)
+                        "password", mock=True, engines=engines, fLOG=noLOG)
         self.assertNotEmpty(js)
 
     def test_jenkins_local27(self):
-        fLOG(
-            __file__,
-            self._testMethodName,
-            OutputPrint=__name__ == "__main__")
-
         modules = default_jenkins_jobs(".*yml.*", ".*update.*")
-        self.assertIn(len(modules), (41, 42))
+        self.assertIn(len(modules), (33, 34, 35))
         modules = default_jenkins_jobs(".*27.*", ".*update.*")
         self.assertEqual(len(modules), 0)
 
